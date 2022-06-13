eb8d796305a85c0435223eee2b67d560c6302199
JavonHuang <44997367+JavonHuang@users.noreply.github.com>
2022-06-13 17:48:18 +0800

    1
    1

--- src/main.js
+++ src/main.js
@@ -43,18 +43,14 @@ export async function bootstrap () {
  * 应用每次进入都会调用 mount 方法，通常我们在这里触发应用的渲染方法
  */
 export async function mount (props) {
-  // props.onGlobalStateChange((state, prev) => {
-  //   // state: 变更后的状态; prev 变更前的状态
-  //   console.log('vue子应用监听父应用', state, prev)
-  // })
-  // props.getGlobalState((e) => {
-  //   console.log('vue子应用初始化获取', e)
-  // })
-  // setTimeout(() => {
-  //   props.setMainVuex('portalModule/cm_fullscreen', true)
-  //   console.log('重置')
-  // }, 2000)
+  const param = { customer: true }
+  props.onGlobalStateChange((state, prev) => {
+    // state: 变更后的状态; prev 变更前的状态
+    console.log('customer son1监听', state, prev)
+  })
   render(props)
+  props.setGlobalState(param)
+  console.log('customer son1初始化获取', props.getGlobalState())
 }
 
 /**
